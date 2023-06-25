a00e8f1faae9b254715ac7d894fa7b730ee9bc9b
Kamran Raza <aipctoner@gmail.com>
2023-06-25 17:06:23 +0500

    edit

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
-
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button =>{
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
         }
-
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
         }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
         }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
         }
 
     })
-})
+}
+
